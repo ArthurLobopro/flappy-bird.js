fc0667317c992906e805757df5eccc956c4b635f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-06 12:53:26 -0300

    módulo com audios

---
+++ src/Audio.js
@@ -0,0 +1,9 @@
+const audios = {}
+
+audios.hit = new Audio()
+audios.hit.src = "./assets/sons/hit.wav"
+
+audios.pulo = new Audio()
+audios.pulo.src = "./assets/sons/pulo.wav"
+
+export default audios
