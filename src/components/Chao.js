7553be54bcddcd3e60ae2e74b76f28c0f6fcf95c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-06 00:33:29 -0300

    Chão infinito

--- src/components/Chao.js
+++ src/components/Chao.js
@@ -6,21 +6,28 @@ const chao = {
     sx: 0,
     sy: 610,
     y: null,
+    x: 0,
+    move: 0,
     draw(){
         ctx.drawImage(
             sprites, //Imagem
             this.sx, this.sy, //Posição do Sprite atual
             this.width,this.height, // Recorte feito no Sprite
-            0, this.y, // Posição no canvas
+            this.x, this.y, // Posição no canvas
             this.width, this.height // Largura e altura no canvas
         )
         ctx.drawImage(
             sprites, 
             this.sx, this.sy, 
             this.width,this.height, 
-            (canvas.width - this. width),(canvas.height - this.height),
+            //(canvas.width - this. width - this.move),(canvas.height - this.height),
+            (this.x + this.width),(canvas.height - this.height),
             this.width, this.height
         )
+    },
+    att(){
+        // Esta forma reseta automaticamente, o segundo parametro define quando será repetido (no caso metade)
+        this.x = (this.x - 0.75) % (this.width / 2)
     }
 }
 chao.y = (canvas.height - chao.height)
