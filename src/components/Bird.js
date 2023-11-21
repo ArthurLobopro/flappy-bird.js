0f58634924056bc8ba42d635aa24d65212b929ca
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-11-21 12:26:27 -0300

    refactor: convert Bird to class

--- src/components/Bird.js
+++ src/components/Bird.js
@@ -1,28 +1,36 @@
+import { Component } from "./Component.js"
 import { ctx, sprites } from "./render.js"
 
-export const bird = {
-    frame: 0,
-    width: 33,
-    height: 24,
-    gravity: 0.25,
-    currentSprite: 0,
-    x: 0,
-    y: 0,
-    fallVelocity: 0,
-    sprites: [
+class BirdComponent extends Component {
+    frame = 0
+    width = 33
+    height = 24
+    gravity = 0.25
+    currentSprite = 0
+    fallVelocity = 0
+    sprites = [
         {
-            sx: 0,
-            sy: 0,
+            sx: 0, sy: 0
         },
         {
-            sx: 0,
-            sy: 26
+            sx: 0, sy: 26
         },
         {
-            sx: 0,
-            sy: 52,
+            sx: 0, sy: 52,
         }
-    ],
+    ]
+
+
+    constructor() {
+        super()
+        this.reset()
+    }
+
+    reset() {
+        this.x = 15
+        this.y = 15
+        this.fallVelocity = 0
+    }
 
     update() {
         this.y += (this.fallVelocity + this.gravity)
@@ -32,32 +40,27 @@ export const bird = {
         if (this.frame % 10 === 0) {
             this.toggle()
         }
-    },
+    }
 
     draw() {
         const { sx, sy } = this.sprites[this.currentSprite]
         const { width, height, x, y } = this
 
         ctx.drawImage(
-            sprites, //Imagem
-            sx, sy, //Posição do Sprite atual
-            width, height, // Recorte feito no Sprite
-            x, y, // Posição no canvas
-            width, height // Largura e altura no canvas
+            sprites,
+            sx, sy,
+            width, height,
+            x, y,
+            width, height
         )
-    },
+    }
 
     toggle() {
         this.currentSprite++
         if (this.currentSprite === 3) {
             this.currentSprite = 0
         }
-    },
-
-    reset() {
-        this.x = 15
-        this.y = 15
-        this.fallVelocity = 0
     }
 }
-bird.reset()
+
+export const bird = new BirdComponent()
---
+++ src/components/Component.js
@@ -0,0 +1,8 @@
+export class Component {
+    x = 0
+    y = 0
+    width = 0
+    height = 0
+
+    draw() { }
+}
